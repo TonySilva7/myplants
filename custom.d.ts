cdaf61bdfddf57bc02be7c0d6d5b0d90c59ed34e
TonySilva7 <tony.deve7oper@gmail.com>
2021-04-21 19:01:04 -0300

    Created Header of the Plantselect screen

--- custom.d.ts
+++ custom.d.ts
@@ -2,3 +2,8 @@ declare module "*.png" {
   const content: any;
   export default content;
 }
+
+declare module "*.jpg" {
+  const content2: any;
+  export default content2;
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {StyleSheet, Text, Image, View} from "react-native";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import colors from "../styles/colors";
+import userImage from "../assets/tony.jpg";
+import fonts from "../styles/fonts";
+
+export function Header() {
+  
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>Olá,</Text>
+        <Text style={styles.userName}>Tony</Text>
+      </View>
+      <Image source={userImage} style={styles.userImage}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 20,
+    marginTop: getStatusBarHeight(),
+    padding: 20,
+  },
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
+  userName: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 40,
+  },
+  userImage: {
+    width: 70,
+    height: 70,
+    borderRadius: 40,
+  }
+});
