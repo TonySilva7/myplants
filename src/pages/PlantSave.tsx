6d360f2bb63f59748065d011720857dbdfad788c
TonySilva7 <tony.deve7oper@gmail.com>
2021-04-23 11:31:01 -0300

    DateTime implemented to water the plant

--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import {Alert, StyleSheet, Text, View, Image, ScrollView, Platform, TouchableOpacity} from "react-native";
 import { getBottomSpace } from "react-native-iphone-x-helper";
 import { useRoute } from "@react-navigation/core";
+import DateTimePicker, { Event } from "@react-native-community/datetimepicker";
+import { isBefore } from "date-fns";
 
 import {SvgFromUri} from "react-native-svg";
 import WaterDrop from "../assets/waterdrop.png";
@@ -29,6 +31,24 @@ export function PlantSave() {
   const route = useRoute();
   const { plant } = route.params as Params;
   
+  // states
+  const [selectedDateTime, setSelectedDateTime] = useState(new Date());
+  const [showDatePicker, setShowDatePicker] = useState(Platform.OS == "ios");
+  
+  // functions
+  function handleChangeTime(event: Event, dateTime: Date | undefined) {
+    if (Platform.OS === "android")
+      setShowDatePicker((oldState) => !oldState);
+    
+    if (dateTime && isBefore(dateTime, new Date())) {
+      setSelectedDateTime(new Date());
+      return Alert.alert("Escolha uma hora no futuro! ⏰");
+    }
+    
+    if (dateTime)
+      setSelectedDateTime(dateTime);
+  }
+  
   return (
     <>
       <View style={styles.container}>
@@ -51,6 +71,8 @@ export function PlantSave() {
         <Text style={styles.alertLabel}>
           Escolha o melhor horário para ser lembrado.
         </Text>
+        
+        <DateTimePicker value={selectedDateTime} mode="time" display="spinner" onChange={handleChangeTime} />
         
         <Button title="Cadastrar Planta" onPress={() => {}}/>
       </View>
