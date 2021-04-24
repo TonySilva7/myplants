eff987081a8b8dc9ecb92044ab72849b44563a73
TonySilva7 <tony.deve7oper@gmail.com>
2021-04-24 15:25:38 -0300

    Refactoring App.tsx

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,13 +1,22 @@
-import { Feather } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { Dimensions, Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import WateringImg from "../assets/watering.png";
+import {
+  SafeAreaView,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  View,
+} from "react-native";
+
+import { Feather } from "@expo/vector-icons";
+
+import wateringImg from "../assets/watering.png";
 import colors from "../styles/colors";
 import fonts from "../styles/fonts";
+import { useNavigation } from "@react-navigation/native";
 
 export function Welcome() {
-  
   const navigation = useNavigation();
   
   function handleStart() {
@@ -15,19 +24,27 @@ export function Welcome() {
   }
   
   return (
-    <SafeAreaView style={ styles.container }>
-      <View style={ styles.wrapper }>
-        <Text style={ styles.title }> Gerencie { '\n' } suas plantas de { '\n' } forma fácil </Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie{"\n"}
+          suas plantas de {"\n"}
+          forma fácil
+        </Text>
+        <Image source={wateringImg} style={styles.image} resizeMode="contain" />
         
-        <Image source={ WateringImg } style={ styles.image } resizeMode="contain"/>
-        
-        <Text style={ styles.subtitle }>
-          Não esqueça mais de regar suas { '\n' } plantas.
-          Nós cuidamos de lembrar você sempre que precisar.
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas {"\n"}
+          plantas. Nós cuidamos de lembrar {"\n"}
+          você sempre que precisar.
         </Text>
         
-        <TouchableOpacity style={ styles.button } activeOpacity={ 0.8 } onPress={handleStart}>
-          <Feather name="chevron-right" style={ styles.buttonIcon }/>
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.7}
+          onPress={handleStart}
+        >
+          <Feather name="chevron-right" style={styles.buttonIcon} />
         </TouchableOpacity>
       </View>
     </SafeAreaView>
@@ -45,21 +62,22 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
   },
   title: {
-    fontFamily: fonts.heading,
-    lineHeight: 32,
-    fontSize: 30,
-    fontWeight: "bold",
+    fontSize: 28,
     textAlign: "center",
     color: colors.heading,
-    marginTop: 38
+    marginTop: 38,
+    fontFamily: fonts.heading,
+    lineHeight: 30,
   },
   subtitle: {
-    fontFamily: fonts.text,
-    lineHeight: 26,
     textAlign: "center",
     fontSize: 18,
     paddingHorizontal: 20,
     color: colors.heading,
+    fontFamily: fonts.text,
+  },
+  image: {
+    height: Dimensions.get("window").width * 0.7,
   },
   button: {
     backgroundColor: colors.green,
@@ -71,10 +89,7 @@ const styles = StyleSheet.create({
     width: 56,
   },
   buttonIcon: {
+    fontSize: 32,
     color: colors.white,
-    fontSize: 30,
-  },
-  image: {
-    height: Dimensions.get("window").width * 0.7,
   },
 });
--- src/routes/tab.routes.tsx
+++ src/routes/tab.routes.tsx
@@ -1,50 +1,51 @@
-import { MaterialIcons } from "@expo/vector-icons";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
-import { MyPlants } from "../pages/MyPlants";
-import { PlantSelect } from "../pages/PlantSelect";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import colors from "../styles/colors";
+import { PlantSelect } from "../pages/PlantSelect";
+import { MaterialIcons } from "@expo/vector-icons";
+import { MyPlants } from "../pages/MyPlants";
+import { Platform } from "react-native";
 
 const AppTab = createBottomTabNavigator();
 
 const AuthRoutes = () => {
   return (
     <AppTab.Navigator
-      tabBarOptions={ {
+      tabBarOptions={{
         activeTintColor: colors.green,
         inactiveTintColor: colors.heading,
         labelPosition: "beside-icon",
         style: {
-          paddingVertical: 20,
+          // paddingVertical: Platform.OS === 'ios' ? 0 : 0,
           height: 88,
         },
-      } }
+      }}
     >
       <AppTab.Screen
-        name={ "Nova Planta" }
-        component={ PlantSelect }
-        options={ {
-          tabBarIcon: (({ size, color }) => (
+        name="Nova Planta"
+        component={PlantSelect}
+        options={{
+          tabBarIcon: ({ size, color }) => (
             <MaterialIcons
               name="add-circle-outline"
-              size={ size }
-              color={ color }
+              size={size}
+              color={color}
             />
-          ))
-        } }
+          ),
+        }}
       />
       <AppTab.Screen
-        name={ "Minhas Plantas" }
-        component={ MyPlants }
-        options={ {
-          tabBarIcon: (({ size, color }) => (
+        name="Minhas plantas"
+        component={MyPlants}
+        options={{
+          tabBarIcon: ({ size, color }) => (
             <MaterialIcons
               name="format-list-bulleted"
-              size={ size }
-              color={ color }
+              size={size}
+              color={color}
             />
-          ))
-        } }
+          ),
+        }}
       />
     </AppTab.Navigator>
   );
